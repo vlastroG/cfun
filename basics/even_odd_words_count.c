a1c23c5c9dedf8aa13315334a05daa6d27c07f35
vlastroG <vlastroganov@gmail.com>
2023-12-14 22:20:27 +0300

    Problem 2.19: a, b

---
+++ basics/even_odd_words_count.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/*
+ *  Program reads input and writes after each input line two numbers:
+ *  first number = sum of words, which consist of even count of letters,
+ *  second number = sum of words, which consist of odd count of letters
+ */
+
+int main() {
+    int c, odd_count, even_count, letters_count;
+    even_count = odd_count = letters_count = 0;
+    while ((c = getchar()) != EOF) {
+        if (c != ' ' && c != '\t' && c != '\n') {
+            letters_count++;
+        } else {
+            if (letters_count) {
+                letters_count % 2 ? odd_count++ : even_count++;
+            }
+            letters_count = 0;
+            if (c == '\n') {
+                printf("Even words count = %d, odd words count = %d\n",
+                       even_count, odd_count);
+                even_count = odd_count = 0;
+            }
+        }
+    }
+}
---
+++ basics/words_count_in_line.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int c, prev_letter_is_space, count;
+    count = 0;
+    prev_letter_is_space = 1;
+    while ((c = getchar()) != EOF) {
+        if (c == '\n') {
+            printf("%d\n", count);
+            prev_letter_is_space = 1;
+            count = 0;
+        } else if (prev_letter_is_space && (c != ' ') && (c != '\t')) {
+            count++;
+            prev_letter_is_space = 0;
+        } else if (c == ' ' || c == '\t') {
+            prev_letter_is_space = 1;
+        } else {
+            continue;
+        }
+    }
+}
